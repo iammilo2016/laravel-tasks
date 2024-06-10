03e881a32fdab6ff6ae7f34cafb682a5a8349d61
iammilo2016 <alanmiloto@gmail.com>
2024-06-09 22:19:38 -0600

    modify

--- config/database.php
+++ config/database.php
@@ -139,12 +139,13 @@ return [
         ],
 
         'cache' => [
+            'url' => env('REDIS_URL'),
             'host' => env('AZURE_REDIS_HOST', '127.0.0.1'),
             'username' => env('REDIS_USERNAME'),
             'password' => env('AZURE_REDIS_PASSWORD'),
             'port' => env('AZURE_REDIS_PORT', '6379'),
             'database' => env('AZURE_REDIS_DATABASE', '1'),
-            'scheme' => 'tls'
+            'scheme' => 'tls',
         ],
 
     ],
