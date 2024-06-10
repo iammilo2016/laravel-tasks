3a56870251223c21209c8ebca63ebf944b33844f
iammilo2016 <alanmiloto@gmail.com>
2024-06-09 22:46:46 -0600

    rollback

--- database/migrations/2014_10_12_100000_create_password_reset_tokens_table.php
+++ database/migrations/2014_10_12_100000_create_password_reset_tokens_table.php
@@ -11,16 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-    // Check if the table does not exist before trying to create it
-    if (!Schema::hasTable('password_reset_tokens')) {
         Schema::create('password_reset_tokens', function (Blueprint $table) {
             $table->string('email')->primary();
             $table->string('token');
             $table->timestamp('created_at')->nullable();
         });
     }
-}
-
     /**
      * Reverse the migrations.
      */
